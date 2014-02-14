50deedc8e35461f6535c626dbe5c70fa01751c61
Ryan Bolt <ryanbolt075@gmail.com>
2014-02-13 21:58:50 -0330

    Start

---
+++ src/Grade/GradeBookTest.java
@@ -0,0 +1,15 @@
+package Grade;
+
+/**
+ * Created by ryanbolt on 2/13/2014.
+ */
+public class GradeBookTest
+{
+    public static void main(String[] args)
+    {
+        GradeBook myGradeBook = new GradeBook("CS101 Intro to Java Programming");
+
+        myGradeBook.displayMessage();
+        myGradeBook.determineClassAverage();
+    }
+}
